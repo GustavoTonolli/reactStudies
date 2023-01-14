6a4b24171f687b929851e31741bf92365c6a0448
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-14 02:12:10 +0000

    FirstLookAtReact

---
+++ react.js
@@ -0,0 +1,15 @@
+// React Framework
+// criado pelo facebook
+//Roda Javascript no servidor e retorna a pagina pronta
+// Baseado em JS ES6
+// O react somente atualiza os componentes que sofreram alteracoes (o que economiza memoria, e consumo de dados);
+
+// trabalha com modelo composicional
+// com varios componentes, cada area seria um componente.
+// menus, barras laterais, area de anuncios. cada um componente separado
+
+// componentes combinados
+// natureza declarativa, tenho algo que faz o processamento para a funcao que necessito por exemplo usando map no lugar de for.
+// Unidirecional, os dados fluem em apenas uma direcao. Os dados vao do componente pai para o componente filho.
+// Tudo e JavaScript
+// JSX nos possibilita escrever HTML junto de Javascript
