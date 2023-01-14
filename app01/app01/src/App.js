87fc885b6033e869c5ca65e0bcd8ac658963c47f
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2023-01-14 03:24:45 +0000

    ReactIntro

--- app01/app01/src/App.js
+++ app01/app01/src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() { // function precisa ter o mesmo nome do App.js
   return (
-    <div className="App">
+    <div className="App"> {/* posso executar um bloco sem necessariamente ter uma tag com <>code</> */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edite <code>src/App.js</code> e salve para recarregar.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,7 +20,8 @@ function App() {
         </a>
       </header>
     </div>
+
   );
 }
 
-export default App;
+export default App; // esta exportando o App como default
--- app01/app01/src/index.js
+++ app01/app01/src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App'; // importando o App.js como App
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = ReactDOM.createRoot(document.getElementById('root')); // Root onde o app sera renderizado
+root.render( // dentro da funcao passa o que sera renderizado em root
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+/* ReactDOM.render( esse era o modo antigo, nao funciona mais!
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+); */
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
